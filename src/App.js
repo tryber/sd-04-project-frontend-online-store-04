8219541cf7ccc14f575d4b16fd808905dda27a7d
Thiago Tavares <thiagooo15@gmail.com>
2020-06-17 18:52:52 -0300

    requisito 2 passando

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Categories from './components/Categories';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Categories />
+      <SearchBar />
     </div>
   );
 }
