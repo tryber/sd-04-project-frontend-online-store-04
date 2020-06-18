615c6da29bd93e588dbf45c3f62d425bab551da8
Thiago Tavares <thiagooo15@gmail.com>
2020-06-18 19:38:17 -0300

    requisito 9

--- src/components/EmptyCart.jsx
+++ src/components/EmptyCart.jsx
@@ -4,10 +4,10 @@ import shoppingcart from '../images/shopping-cart.jpg';
 export default class Shopping extends React.Component {
   render() {
     return (
-      <div>
+      <div data-testid="shopping-cart-empty-message">
         <h3>Carrinho de Compras</h3>
         <img src={shoppingcart} alt="cart" />
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        <p>Seu carrinho está vazio</p>
       </div>
     );
   }
