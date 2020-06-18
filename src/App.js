ac71876cf5570e0912542f91e812c23afe41e956
Thiago Tavares <thiagooo15@gmail.com>
2020-06-18 18:22:44 -0300

    requisito 9

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ import * as pages from './pages';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <div className="container-bar-cart">
-        <SearchBar className='search-bar'/>
-        <ShoppingCart className='shopping-cart' />
-      </div>
-      <Categories className='categories'/>
-=======
     <div>
       <Router>
         <Switch>
@@ -21,7 +13,6 @@ function App() {
           <Route component={pages.NotFound} />
         </Switch>
       </Router>
->>>>>>> b554d9303a96af8ce35466a6e0118afe2a27fb1c
     </div>
   );
 }
