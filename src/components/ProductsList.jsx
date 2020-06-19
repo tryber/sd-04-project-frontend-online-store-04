7fc355c99757144fb8978e173c1d58e4c298141a
orlando-messias <programadorlando@gmail.com>
2020-06-19 15:13:53 -0300

    procuts and products details

---
+++ src/components/ProductsList.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class ProductsList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
+
+  render() {
+    const { products } = this.props;
+    if(!products) return <div></div>
+    return(
+      <div>
+        {products.map(({ id, title, thumbnail, price, available_quantity }) => (
+          <div data-testid="product" key={id}>
+            <p>{title}</p>
+            <img src={thumbnail} alt="produto" />
+            <p>{`R$ ${price.toFixed(2)}`}</p>
+              <Link to={{pathname:`/details/${id}`, state:{title, thumbnail, price, available_quantity}}}>
+                <p data-testid="product-detail-link">Ver Detalhes</p>
+              </Link>
+          </div>
+          
+        ))}
+        
+      </div> 
+    )
+  }
+}
+
+export default ProductsList;
