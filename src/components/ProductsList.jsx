56f89acc95cb2344f10088206745593c1a618ca8
orlando-messias <programadorlando@gmail.com>
2020-06-19 19:49:24 -0300

    requisito 08

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ShoppingCart from './ShoppingCart';
+import './categories.css';
 
 class ProductsList extends Component {
   constructor(props) {
     super(props);
-    this.state = { };
+    this.state = { cartItems:[] };
+    this.addToCart = this.addToCart.bind(this);
+  }
+
+  addToCart(product) {
+    this.setState((state) => ({cartItems: [...state.cartItems, product]}));
   }
 
   render() {
     const { products } = this.props;
     if(!products) return <div></div>
     return(
+      <div className="categories-products">
       <div>
+        <button data-testid="shopping-cart-button">Teste</button>
         {products.map(({ id, title, thumbnail, price, available_quantity }) => (
           <div data-testid="product" key={id}>
             <p>{title}</p>
@@ -20,10 +29,13 @@ class ProductsList extends Component {
               <Link to={{pathname:`/details/${id}`, state:{title, thumbnail, price, available_quantity}}}>
                 <p data-testid="product-detail-link">Ver Detalhes</p>
               </Link>
+              <p data-testid="product-add-to-cart" onClick={() => this.addToCart({id, title, thumbnail, price, available_quantity})}>Adicionar ao Carrinho</p>
           </div>
-          
         ))}
-        
+        <div>
+      </div>
+          <ShoppingCart cartItems={this.state.cartItems} />
+        </div>
       </div> 
     )
   }
--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import EmptyCart from './EmptyCart';
 
-export default class Shopping extends React.Component {
+export default class ShoppingCart extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { items: [] };
+    this.state = { items: this.props.cartItems };
   }
 
   render() {
-    if (this.state.items.length === 0) return <EmptyCart />;
+    const {cartItems} = this.props
+
+    if (cartItems.length < 1) return <EmptyCart />;
     return (
       <div>
         <h3>Carrinho de Compras</h3>
+        {cartItems.map((item) => (
+          <div key={item.id}>
+            <p data-testid="shopping-cart-product-name">{item.title}</p>
+            <p data-testid="shopping-cart-product-quantity">1</p>
+          </div>
+        ))}
       </div>
     );
   }
