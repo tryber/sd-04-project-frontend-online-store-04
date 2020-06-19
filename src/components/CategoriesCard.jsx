fd42eb2fe8493b64fe21ebc619f428a349c68358
Thiago Tavares <thiagooo15@gmail.com>
2020-06-19 15:40:45 -0300

    cc fix2

--- src/components/CategoriesCard.jsx
+++ src/components/CategoriesCard.jsx
@@ -29,7 +29,7 @@ class CategoriesCard extends Component {
           </label>
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -4,28 +4,31 @@ import { Link } from 'react-router-dom';
 class ProductsList extends Component {
   constructor(props) {
     super(props);
-    this.state = { };
+    this.state = {};
   }
 
   render() {
     const { products } = this.props;
-    if(!products) return <div></div>
-    return(
+    if (!products) return <div />;
+    return (
       <div>
         {products.map(({ id, title, thumbnail, price, available_quantity }) => (
           <div data-testid="product" key={id}>
             <p>{title}</p>
             <img src={thumbnail} alt="produto" />
             <p>{`R$ ${price.toFixed(2)}`}</p>
-              <Link to={{pathname:`/details/${id}`, state:{title, thumbnail, price, available_quantity}}}>
-                <p data-testid="product-detail-link">Ver Detalhes</p>
-              </Link>
+            <Link
+              to={{
+                pathname: `/details/${id}`,
+                state: { title, thumbnail, price, available_quantity },
+              }}
+            >
+              <p data-testid="product-detail-link">Ver Detalhes</p>
+            </Link>
           </div>
-          
         ))}
-        
-      </div> 
-    )
+      </div>
+    );
   }
 }
 
