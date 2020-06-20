237273b3dcc40369e88ee700c4bd37b0b0497b12
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-06-20 17:18:53 -0300

    Requisitos 1 ao 9 e 11

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,32 +1,46 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ShoppingCart from './ShoppingCart';
 
 class ProductsList extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { cartItems: [] };
+    this.addToCart = this.addToCart.bind(this);
+  }
+
+  addToCart(product) {
+    this.setState((state) => ({ cartItems: [...state.cartItems, product] }));
   }
 
   render() {
     const { products } = this.props;
+    const { cartItems } = this.state;
     if (!products) return <div />;
     return (
-      <div>
+      <div className="categories-products">
         {products.map(({ id, title, thumbnail, price, available_quantity }) => (
           <div data-testid="product" key={id}>
             <p>{title}</p>
-            <img src={thumbnail} alt="product" />
+            <img src={thumbnail} alt="produto" />
             <p>{`R$ ${price.toFixed(2)}`}</p>
             <Link
               to={{
                 pathname: `/details/${id}`,
-                state: { id, title, thumbnail, price, available_quantity },
+                state: { title, thumbnail, price, available_quantity, cartItems },
               }}
             >
               <p data-testid="product-detail-link">Ver Detalhes</p>
             </Link>
+            <button
+              data-testid="product-add-to-cart"
+              onClick={() => this.addToCart({ id, title, thumbnail, price, available_quantity })}
+            >
+              Adicionar ao Carrinho
+            </button>
           </div>
         ))}
+        <ShoppingCart cartItems={cartItems} />
       </div>
     );
   }
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,36 +1,45 @@
 import React, { Component } from 'react';
+import ShoppingCart from '../components/ShoppingCart';
 
 class Details extends Component {
   constructor(props) {
     super(props);
-    this.state = { product: '' };
+    this.state = { cartItems: this.props.location.state.cartItems };
+    this.addToCart = this.addToCart.bind(this);
+  }
+
+  addToCart(product) {
+    this.setState((state) => ({ cartItems: [...state.cartItems, product] }));
   }
 
   render() {
-    const { id, title, thumbnail, price, availableQuantity } = this.props.location.state;
+    const { availableQuantity } = this.props.location.state.available_quantity;
+    const { id, title, thumbnail, price } = this.props.location.state;
     return (
       <div>
+        <h2>{id}</h2>
+        <h3 data-testid="product-detail-name">{title}</h3>
+        <img src={thumbnail} alt="product" />
+        <h3>{price}</h3>
+        <h4>{availableQuantity}</h4>
+        <button
+          data-testid="product-detail-add-to-cart"
+          onClick={() => this.addToCart({ id, title, thumbnail, price, availableQuantity })}
+        > Adicionar ao Carrinho
+        </button>
         <div>
-          <h2>{id}</h2>
-          <h3 data-testid="product-detail-name">{title}</h3>
-          <img src={thumbnail} alt="product" />
-          <h3>{price}</h3>
-          <h4>{availableQuantity}</h4>
+          <ShoppingCart cartItems={this.state.cartItems} />
+          <button style={{ display: 'none' }} data-testid="shopping-cart-button" />
         </div>
         <div>
-          <form>
-            <h2>Avaliações</h2>
-            <label htmlFor="inputEmail">Email:</label><br />
-            <input type="email" />
-            <br />
-            <label htmlFor="inputText">Mensagem:</label><br />
-            <input type="text" data-testid="product-detail-evaluation" />
-            <br />
-            <label htmlFor="inputRating">Dê sua nota ao produto:</label><br />
-            <input type="number" min="1" max="5" />
-            <br />
-            <button>Avaliar</button>
-          </form>
+          <h2>Avaliações</h2>
+          <label htmlFor="inputEmail">Email:</label><br />
+          <input type="email" /><br />
+          <label htmlFor="inputText">Mensagem:</label><br />
+          <input type="text" data-testid="product-detail-evaluation" /><br />
+          <label htmlFor="inputRating">Dê sua nota ao produto:</label><br />
+          <input type="number" min="1" max="5" /><br />
+          <button>Avaliar</button>
         </div>
       </div>
     );
@@ -38,3 +47,4 @@ class Details extends Component {
 }
 
 export default Details;
+
